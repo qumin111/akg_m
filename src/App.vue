9b9e31add054b273629f748f4e07e0d6ffa56c57
qu min <qumin186@163.com>
2019-10-04 13:04:06 +0800

    'href'

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ export default {
     let flag = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)
 
     if (!flag) {
-      window.location.href = 'http://localhost:8081/#/home';
+      window.location.href = 'http://m.akgvc.com/';
     }
   },
 }
